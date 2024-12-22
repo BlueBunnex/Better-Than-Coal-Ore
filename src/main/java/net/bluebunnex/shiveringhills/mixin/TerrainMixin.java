0686b360d68f8823058a4bda6146ff0c590dbe6d
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2024-12-22 06:30:57 -0500

    im gonna be real these push messages may not be very good

---
+++ src/main/java/net/bluebunnex/shiveringhills/mixin/TerrainMixin.java
@@ -0,0 +1,9 @@
+package net.bluebunnex.shiveringhills.mixin;
+
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Minecraft.class)
+public class TerrainMixin {
+
+}
