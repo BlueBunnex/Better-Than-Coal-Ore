b7c6c6d15f8a67774cdf43b7646fa41132bc534c
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-01-11 16:13:59 -0500

    dirt and sand piles

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # Better Than Coal Ore
+Challenge Mod for b1.7.3 with an actual end goal
 
 > [!WARNING]
 > I need to change the project title
 
-- Challenge Mod for b1.7.3 with an actual end goal
+# TODO
+- sand and dirt now drop sand and dirt piles, and a chance of a rock (sand's chance is higher)
+
+## Plan
 - adds dark knights to the nether that guard a structure that contains an item you need to summon the final boss
 - and it's forced hardcore
-- sand and dirt now drop sand and dirt piles, and a chance of a rock (sand's chance is higher)
+- iron doesn't spawn on the surface
--- src/main/java/net/bluebunnex/btco/BetterThanCoalOre.java
+++ src/main/java/net/bluebunnex/btco/BetterThanCoalOre.java
@@ -1,8 +1,10 @@
 package net.bluebunnex.btco;
 
 import net.mine_diver.unsafeevents.listener.EventListener;
+import net.minecraft.item.Item;
 import net.modificationstation.stationapi.api.event.registry.ItemRegistryEvent;
 import net.modificationstation.stationapi.api.mod.entrypoint.Entrypoint;
+import net.modificationstation.stationapi.api.template.item.TemplateItem;
 import net.modificationstation.stationapi.api.util.Namespace;
 import net.modificationstation.stationapi.api.util.Null;
 
@@ -11,9 +13,16 @@ public class BetterThanCoalOre {
     @Entrypoint.Namespace
     public static final Namespace NAMESPACE = Null.get();
 
+    public static Item DIRT_PILE;
+    public static Item SAND_PILE;
+
     @EventListener
     public void registerItems(ItemRegistryEvent event) {
 
-        System.out.println("Registering items for " + NAMESPACE.getName());
+        DIRT_PILE = new TemplateItem(NAMESPACE.id("dirt_pile"))
+                .setTranslationKey(NAMESPACE, "dirt_pile");
+
+        SAND_PILE = new TemplateItem(NAMESPACE.id("sand_pile"))
+                .setTranslationKey(NAMESPACE, "sand_pile");
     }
 }
